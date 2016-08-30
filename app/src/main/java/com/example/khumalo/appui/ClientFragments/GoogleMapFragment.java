78a9aa140afd16fcf6594331d56af2ba6c2df02e
ComfortC <comfortchino@gmail.com>
2016-08-30 22:00:31 +0200

    Client MapFragment Inserted

--- app/src/main/java/com/example/khumalo/appui/ClientFragments/GoogleMapFragment.java
+++ app/src/main/java/com/example/khumalo/appui/ClientFragments/GoogleMapFragment.java
@@ -3,11 +3,17 @@ package com.example.khumalo.appui.ClientFragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.khumalo.appui.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +25,13 @@ import com.example.khumalo.appui.R;
 public class GoogleMapFragment extends Fragment {
 
 
+    private SupportMapFragment mSupportMapFragment;
+    GoogleMap m_map;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-
      * @return A new instance of fragment GoogleMapFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -42,25 +50,42 @@ public class GoogleMapFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-                   }
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_google_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_google_map, container, false);
+        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.map, mSupportMapFragment).commit();
+        }
+
+        if (mSupportMapFragment != null) {
+            mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    m_map = googleMap;
+                }
+            });
+
+
+        }
+        return rootView;
     }
 
 
+        @Override
+        public void onDestroy () {
+            super.onDestroy();
 
+        }
 
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
 
     }
 
-}
